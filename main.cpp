a4e4f2395e6abf927bd4af0d1c8919066ad45686
Anton <kilantt@gmail.com>
2021-01-13 10:56:00 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <QCoreApplication>
+#include <highgui.hpp>
+#include <types_c.h>
+#include <mat.hpp>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+
+    return a.exec();
+}
