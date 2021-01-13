aca470b19b37f455db06d97891933fcf27f07bf4
Anton <kilantt@gmail.com>
2021-01-13 23:44:08 +0500

    first commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <highgui.hpp>
 #include <types_c.h>
 #include <mat.hpp>
+//#include <imgcodecs/legacy/constants_c.h>
+//#include <imgcodecs/imgcodecs_c.h>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,11 +11,32 @@
 
 
 using namespace cv;
+using namespace std;
+
+IplImage* image = 0;
+IplImage* src = 0;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+//    QString filename = "image.jpg";
+//    QString file_path;
+//    QTextStream cout(stdout);
+//    QTextStream cin(stdin);
+    // получаем картинку
+//    cout<<"Enter your image directory"<< &Qt::endl(cout);
+//    cin << file_path;
+//    qDebug()<<"ooo";
+//    image = cvLoadImage(filename,1);
 
+//    return 0;
+    QCoreApplication a(argc, argv);
+    QTextStream out(stdout);
+
+    QTextStream inp(stdin);
+    QString str = "";
+    inp >> str;
+    out <<  str << Qt::endl;
 
     return a.exec();
+
 }
